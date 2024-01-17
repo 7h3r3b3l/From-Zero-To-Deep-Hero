1b9bfbec33c2fe787c8b93ad4104375a50741f29
7h3r3b3l <7h3r3b3l@proton.me>
2024-01-17 03:03:54 -0500

    notes taken

---
+++ 0x01-D2L/0x02-Preliminaries/0x01-Data-Manipulation/notes.py
@@ -0,0 +1,107 @@
+"""
+                               Made By
+                               7h3r3b3l
+                ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⡶⢶⣆⠀⠶⣇⢀⣾⡇⢠⣿⢂⣰⡾⢀⣶⡶⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⢀⣤⣾⣧⣂⠻⣧⡨⠻⣷⠀⣿⠮⢹⠇⢸⡿⣸⡿⠁⣿⡟⣤⡿⠁⣨⠿⣶⣄⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⣴⣿⣿⠋⠻⣷⣤⣰⠝⡐⢝⠀⡘⠀⡈⠀⠀⠃⠘⢱⢀⢿⠠⠿⣱⠟⢩⣤⣾⣿⣷⣄⠀⠀⠀⠀
+                ⠀⠀⢀⣾⣿⣿⣏⡉⣣⣊⠙⢓⠃⡘⠈⠀⢁⠀⠆⢀⠀⠡⠂⠄⠰⠈⠀⠙⠡⣃⣉⢙⡿⠿⣿⣿⣧⠀⠀⠀
+                ⠀⠀⣾⣿⣿⠄⠬⠭⠭⣽⠚⠀⠃⠌⠀⡄⢸⠀⡀⠀⠀⠇⠈⠠⠁⠤⠐⠈⠝⢉⡭⠗⢲⣤⣿⣿⣿⣧⠀⠀
+                ⠀⢠⣿⣿⣿⣶⣿⠂⠶⠀⢄⢣⠁⣄⠰⠄⢈⠂⡱⢀⠀⠀⠇⠀⠀⠀⠤⠴⠐⠂⢐⡥⢭⣥⣨⣿⣿⣿⠀⠀
+                ⠀⢸⣿⣿⢉⣙⣛⣻⠛⣛⠀⠊⠑⠄⠈⠀⠐⠀⠀⠘⠀⠀⠠⠂⠒⡉⠀⡀⠎⠤⠀⣤⣄⣽⣿⣿⣿⣿⠀⠀
+                ⠀⢸⣿⣿⣿⠿⠿⠀⣈⣋⠱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠤⠁⠬⠂⣌⣛⠛⣿⣿⣿⣿⠀⠀
+                ⠀⢸⣿⣿⣿⣶⣿⠟⢟⣭⣴⠶⣁⢫⠀⢀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠈⠀⣿⣿⣷⣤⣽⣿⣿⣿⣿⣿⠀⠀
+                ⠀⢸⣿⣿⣿⣿⣿⣷⣿⣿⣇⣼⣿⡘⢰⣛⡆⠀⠐⠡⣞⡧⠀⠀⠀⣀⠘⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+                ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡠⠍⠀⡀⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠿⣿⣿⣿⣿⣿⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠹⣿⣿⣿⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠀⠀⣿⣿⣿⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⣿⣿⡏⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⣿⣿⠁⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠀⢀⣀⠀⠀⠀⠂⠀⠀⠀⡿⠃⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀
+
+Secction 2.1 - Getting Started
+d2l.ai
+"""
+
+# 2.11 - Getting Started
+import torch
+
+## Creating tensors
+x1 = torch.arange(12, dtype=torch.float64)
+x2 = torch.arange(15, dtype=torch.int64)
+x3 = torch.arange(123, dtype=torch.float16)
+
+# Number of elements
+num1 = x1.numel()
+num2 = x1.numel()
+num3 = x3.numel()
+
+# Shapes
+s1 = x1.shape
+s2 = x2.shape
+s3 = x3.shape
+
+# Reshape 
+X1 = x1.reshape(3,4)
+X2 = x2.reshape(3,5)
+X3 = x3.reshape(41,3)
+
+# Zeros Tensor 
+z1 = torch.zeros(10)
+z2 = torch.zeros((2,10))
+
+# Ones Tensor
+o1 = torch.ones(10)
+o2 = torch.ones((41,3))
+
+# Random Normal tensor between 0 and 1
+rn1 = torch.randn(10)
+rn2 = torch.randn((10,2))
+
+# Custom Tensor
+custom_tensor1 = torch.tensor([[1,3,3,7],[1,3,3,7],[1,3,3,7]])
+custom_tensor2 = torch.tensor([[1,3,3,7],[2,3,3,7],[3,3,3,7]])
+
+# Indexing and Slicing
+first_line = custom_tensor1[0, 0:]
+single_element = custom_tensor1[1,3]
+
+## Operations
+
+# e^tensor
+torch.exp(custom_tensor1)
+
+# Compare tensors element by element
+comp = custom_tensor1 == custom_tensor2
+
+# Sum the elements
+sum1 = custom_tensor1.sum()
+sum2 = custom_tensor2.sum()
+
+# Sum
+t_sum = custom_tensor1 + custom_tensor2
+
+## Saving Memory
+
+# Bad Way
+X = custom_tensor2 
+Y = custom_tensor1 
+id_1 = id(X)
+X = X+Y
+id_2 = id(X)
+id_1 == id_2 # False
+
+# Good Way
+X = custom_tensor2 
+Y = custom_tensor1 
+id_1 = id(X)
+X[:] = X+Y # X[:] solves everything.
+id_2 = id(X)
+id_1 == id_2 # True
+
+## Convertions
+A = X.numpy()
+B = torch.from_numpy(A)
+A # Array
+B # Tensor
