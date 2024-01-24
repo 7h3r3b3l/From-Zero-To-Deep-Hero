3c161fef75235f641589e03395d4f8be8b6f448b
7h3r3b3l <7h3r3b3l@proton.me>
2024-01-24 13:31:13 -0500

    advanced in the notes for object oriented programming implementation in deep learning libraries

---
+++ 0x01-D2L/0x03-Linear-Networks-For-Regression/0x02-Object-Oriented-Design-For-Implementation/notes.py
@@ -0,0 +1,66 @@
+"""
+               Made By
+               7h3r3b3l
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⡶⢶⣆⠀⠶⣇⢀⣾⡇⢠⣿⢂⣰⡾⢀⣶⡶⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣤⣾⣧⣂⠻⣧⡨⠻⣷⠀⣿⠮⢹⠇⢸⡿⣸⡿⠁⣿⡟⣤⡿⠁⣨⠿⣶⣄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣴⣿⣿⠋⠻⣷⣤⣰⠝⡐⢝⠀⡘⠀⡈⠀⠀⠃⠘⢱⢀⢿⠠⠿⣱⠟⢩⣤⣾⣿⣷⣄⠀⠀⠀⠀
+⠀⠀⢀⣾⣿⣿⣏⡉⣣⣊⠙⢓⠃⡘⠈⠀⢁⠀⠆⢀⠀⠡⠂⠄⠰⠈⠀⠙⠡⣃⣉⢙⡿⠿⣿⣿⣧⠀⠀⠀
+⠀⠀⣾⣿⣿⠄⠬⠭⠭⣽⠚⠀⠃⠌⠀⡄⢸⠀⡀⠀⠀⠇⠈⠠⠁⠤⠐⠈⠝⢉⡭⠗⢲⣤⣿⣿⣿⣧⠀⠀
+⠀⢠⣿⣿⣿⣶⣿⠂⠶⠀⢄⢣⠁⣄⠰⠄⢈⠂⡱⢀⠀⠀⠇⠀⠀⠀⠤⠴⠐⠂⢐⡥⢭⣥⣨⣿⣿⣿⠀⠀
+⠀⢸⣿⣿⢉⣙⣛⣻⠛⣛⠀⠊⠑⠄⠈⠀⠐⠀⠀⠘⠀⠀⠠⠂⠒⡉⠀⡀⠎⠤⠀⣤⣄⣽⣿⣿⣿⣿⠀⠀
+⠀⢸⣿⣿⣿⠿⠿⠀⣈⣋⠱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠤⠁⠬⠂⣌⣛⠛⣿⣿⣿⣿⠀⠀
+⠀⢸⣿⣿⣿⣶⣿⠟⢟⣭⣴⠶⣁⢫⠀⢀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠈⠀⣿⣿⣷⣤⣽⣿⣿⣿⣿⣿⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣷⣿⣿⣇⣼⣿⡘⢰⣛⡆⠀⠐⠡⣞⡧⠀⠀⠀⣀⠘⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡠⠍⠀⡀⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠿⣿⣿⣿⣿⣿⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠹⣿⣿⣿⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠀⠀⣿⣿⣿⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⣿⣿⡏⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⣿⣿⠁⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠀⢀⣀⠀⠀⠀⠂⠀⠀⠀⡿⠃⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀
+
+Secction 3.2 - Object Oriented Design for implementation
+d2l.ai
+"""
+from d2l import torch as d2l
+from torch import nn
+import numpy as np
+import time
+import torch
+
+# Three classes: 1. Module, 2. Data Module, 3. Data validator
+
+# Module - Contains models, losses and optimization methods
+# DataModule - Contains data and validator
+# Trainer - train the model with the data loader
+
+
+## Define a wrapper that adds a function into a class
+
+# Define the wrapper
+def add_to_class(Class):
+    """
+    This is a method that led register a function as a
+    function for a class outside of the class
+    """
+    def wrapper(obj):
+        # Just add the attritute to the class
+        setattr(Class, obj.__name__, obj)
+    return wrapper
+
+# Define the function
+
+class Lol:
+    def __init__(self, var):
+        self.var = var
+
+# Define a function outside of the class and add it using the wrapper
+
+@add_to_class(Lol)
+def printvar(self):
+    print(f'var of {self.__ne__} is {self.var}')
+    return self.var
+
+obj = Lol(10)
+obj.printvar()
